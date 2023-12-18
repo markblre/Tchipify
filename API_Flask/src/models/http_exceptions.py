a4c18f610f02491dd3571999af59686ca02b058c
lena-cbrd <129036081+lena-cbrd@users.noreply.github.com>
2023-12-18 17:14:10 +0100

    Import de l'exemple de l'API flask

---
+++ API_Flask/src/models/http_exceptions.py
@@ -0,0 +1,24 @@
+
+"""erreur http"""
+class UnprocessableEntity(Exception):
+    pass
+
+
+class NotFound(Exception):
+    pass
+
+
+class Conflict(Exception):
+    pass
+
+
+class Unauthorized(Exception):
+    pass
+
+
+class Forbidden(Exception):
+    pass
+
+
+class SomethingWentWrong(Exception):
+    pass
